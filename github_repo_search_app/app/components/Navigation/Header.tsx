0767db06e09625e32643dadd239254242c18581f
thelearner411 <91204524+thelearner411@users.noreply.github.com>
2024-08-22 10:13:33 +0200

    main layout with header, footer and blank page content

---
+++ github_repo_search_app/app/components/Navigation/Header.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link"
+import { FaGithub } from "react-icons/fa";
+
+export default function Header() {
+    return (
+        <>
+            <div className={`bg-green-main bg-gradient-to-r from-green-main to-blue-main text-green-light p-4`}>
+                <Link href={"/"} className={`block text-center text-green-light text-lg font-bold`}>
+                    <div className={`flex justify-center items-center gap-2`}>
+                        <div>
+                            <FaGithub />
+                        </div>
+                        <span>GITHUB REPOSITORY SEARCH</span>
+                    </div>
+                </Link>
+            </div>
+        </>
+    );
+}
