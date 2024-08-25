9332830ce99ede5c1d08d9e601fdf6dacc2c0340
thelearner411 <91204524+thelearner411@users.noreply.github.com>
2024-08-25 18:03:53 +0200

    rename error pages

--- github_repo_search_app/app/error.tsx
+++ github_repo_search_app/app/error.tsx
@@ -3,7 +3,7 @@ import { FaUserAstronaut } from "react-icons/fa";
 import { CgSpinnerTwo } from "react-icons/cg";
 import Error from "@/app/components/Errors/Error"
 
-export default function NotFound() {
+export default function ErrorPage() {
     return (
         <>
             <div>
--- github_repo_search_app/app/not-found.tsx
+++ github_repo_search_app/app/not-found.tsx
@@ -3,7 +3,7 @@ import { FaUserAstronaut } from "react-icons/fa";
 import { CgSpinnerTwo } from "react-icons/cg";
 import Error404 from "@/app/components/Errors/Error404"
 
-export default function NotFound() {
+export default function NotFoundPage() {
     return (
         <>
             <div>
